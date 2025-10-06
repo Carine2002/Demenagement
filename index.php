d41b4ee9f4b3642cfdefb695a4c84b107ba497d3
Carine2002 <pegdwende.guebre@groupe-esigelec.org>
2025-10-06 09:58:28 +0200

    Création de la page d'accueil

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mon premier script PHP</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "Bonjour!";
+        ?>
+    </h1>
+</body>
+</html>
