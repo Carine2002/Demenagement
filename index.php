0831f64f43c941e2f004062b51e3de64943e92e5
Carine2002 <pegdwende.guebre@groupe-esigelec.org>
2025-11-02 17:32:15 +0100

    index.php

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Mon premier script PHP</title>
-</head>
-<body>
-    <h1>
-        <?php
-            echo "Bonjour!";
-        ?>
-    </h1>
+<?php include_once'header.php';?>
+<?php include_once'nav.php';?>
+<body class="couleur-fond">
+    <div class="container">
+        <div class="row mt-5">
+                <p class="fs-1 fw-bolder text-center mb-1 text-break">Déménagez ensemble,<span class="text-couleur">simplement et solidairement<span></p>
+         </div>
+         <div class="row">
+            <p class="text-break text-center fs-5">La plateforme collaborative qui met en relation les particuliers pour des déménagements réussis.Trouvez de l'aide ou proposez vos services.</p>
+        </div>
+        <div class="row justify-content-center text-center my-3">
+                    <div class=" col-12 col-md-6 ">
+                        <a class=" btn couleur-bouton text-light px-5 fs-6" href="#">Je cherche des déménageurs </a>
+                    </div>
+                    <div class="col-12 col-md-6 ">
+                        <a class=" btn couleur-bouton0 px-5 fs-6 d-inlin-block g-col-6 g-col-sm-12" href="#">Je veux aider à déménager </a>
+                    </div>
+            </div>
+        </div>
 </body>
-</html>
