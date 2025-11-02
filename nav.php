aba61ced16e7c87b6ea892de7b2595e22ea2df14
Carine2002 <pegdwende.guebre@groupe-esigelec.org>
2025-11-02 17:07:03 +0100

    nav.php

---
+++ nav.php
@@ -0,0 +1,20 @@
+<?php include_once'header.php'?>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand text-black" href="#">EasyMove</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link btn px-4 mx-4 couleur-bouton0" href="#">Connexion</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link btn couleur-bouton text-light px-4 mx-4" href="#">S'inscrire</a>
+                    </li>
+                </ul>
+            </div>
+       </div>
+</nav>
