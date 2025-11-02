0ac7a01e25dc73a5b29add3be64ea27a873d461a
Carine2002 <pegdwende.guebre@groupe-esigelec.org>
2025-11-02 20:30:46 +0100

    index.php

--- index.php
+++ index.php
@@ -1,20 +1,57 @@
 <?php include_once'header.php';?>
 <?php include_once'nav.php';?>
 <body class="couleur-fond">
-    <div class="container">
-        <div class="row mt-5">
+    <section>
+        <div class="container">
+            <div class="row mt-5 mb-3">
                 <p class="fs-1 fw-bolder text-center mb-1 text-break">Déménagez ensemble,<span class="text-couleur">simplement et solidairement<span></p>
-         </div>
-         <div class="row">
+            </div>
+        <div class="row my-4">
             <p class="text-break text-center fs-5">La plateforme collaborative qui met en relation les particuliers pour des déménagements réussis.Trouvez de l'aide ou proposez vos services.</p>
         </div>
         <div class="row justify-content-center text-center my-3">
-                    <div class=" col-12 col-md-6 ">
-                        <a class=" btn couleur-bouton text-light px-5 fs-6" href="#">Je cherche des déménageurs </a>
+                    <div class=" col-12 col-md-auto p-2 ">
+                        <a class=" btn couleur-bouton text-light px-5 fs-6  text-nowrap " href="#">Je cherche des déménageurs </a>
                     </div>
-                    <div class="col-12 col-md-6 ">
-                        <a class=" btn couleur-bouton0 px-5 fs-6 d-inlin-block g-col-6 g-col-sm-12" href="#">Je veux aider à déménager </a>
+                    <div class="col-12 col-md-auto p-2  ">
+                        <a class=" btn couleur-bouton0 px-5 fs-6 text-nowrap " href="#">Je veux aider à déménager </a>
                     </div>
             </div>
         </div>
+    </section>
+    <section>
+        <div class="container">
+            <div class="row">
+                <h1 class="py-3 text-center">Comment ça marche ?</h1>
+            </div>
+            <div class="row">
+                <div class="col-12 col-md-4 py-2">
+                    <div class="card rondeur">
+                        <div class="card-body">
+                            <img src="photo/photo1.png" alt="calendrier" class=" py-3">
+                            <h5 class="card-title">1.Créez votre annonce</h5>
+                            <p class="card-text">Décrivez votre déménagement :date, volume,  nombre de personnes nécessaires. Ajoutez des photos pour une meilleure estimation.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4 py-2">
+                    <div class="card rondeur">
+                        <div class="card-body">
+                            <img src="photo/photo2.png" alt="personne" class="py-3">
+                            <h5 class="card-title">2.Recevez des propositions</h5>
+                            <p class="card-text">Les déménageurs intéressés vous proposent un prix. Consultez leurs profils et choisissez les personnes qui vous conviennent</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4 py-2">
+                    <div class="card rondeur">
+                        <div class="card-body">
+                            <img src="photo/photo3.png" alt="securité" class="py-3">
+                            <h5 class="card-title">3.Déménagez sereinement</h5>
+                            <p class="card-text">une fois vos déménageurs sélectionnés, organisez votre déménagement en toute confiance grace à notre plateforme sécurisée</p>
+                        </div>
+                    </div>
+                </div>
+</div>
+
 </body>
